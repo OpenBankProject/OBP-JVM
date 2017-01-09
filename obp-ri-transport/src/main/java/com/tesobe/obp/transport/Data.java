0890134411c4b710371e5e23aa64e66b0f04d4c3
kassapo <ub@kassapo.com>
2017-01-09 14:56:12 +0100

    Data is Serializable

--- obp-ri-transport/src/main/java/com/tesobe/obp/transport/Data.java
+++ obp-ri-transport/src/main/java/com/tesobe/obp/transport/Data.java
@@ -1,18 +1,19 @@
 /*
- * Copyright (c) TESOBE Ltd.  2016. All rights reserved.
+ * Copyright (c) TESOBE Ltd.  2017. All rights reserved.
  *
  * Use of this source code is governed by a GNU AFFERO license that can be found in the LICENSE file.
  *
  */
 package com.tesobe.obp.transport;
 
+import java.io.Serializable;
 import java.math.BigDecimal;
 import java.time.ZonedDateTime;
 
 /**
  * Implemented by {@link Decoder} to read data from the transport.
  */
-public interface Data
+public interface Data extends Serializable
 {
   /**
    * Read a string valued field from the transport
